5c92c1a370ddcef23cb9ce19e2f78e17220122af
Travis Kehrli <tmktahu@gmail.com>
2024-09-23 16:02:48 -0700

    trying to make it work in the browser

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,11 +1,16 @@
-const path = require('path')
-/**
- * @private
- */
+const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+let path, __dirname;
+
+if (isNode) {
+    // Only require 'path' and use __dirname in Node.js
+    path = require('path');
+    __dirname = __dirname || process.cwd();
+}
+
 module.exports = {
-    localConfig: path.join(__dirname, `../../esi.json`),
-    projectConfig: path.join(__dirname, '../../../esi.json'),
-    projectPath: path.join(__dirname, `../../../../`),
+    localConfig: isNode ? path.join(__dirname, `../../esi.json`) : '/esi.json', // Browser uses relative URL
+    projectConfig: isNode ? path.join(__dirname, '../../../esi.json') : '/esi.json', // Adjust for browser
+    projectPath: isNode ? path.join(__dirname, `../../../../`) : '/', // Root or relative path for browser
     routes: ['latest', 'v1', 'legacy', 'dev'],
     server: 'esi.evetech.net'
-}
+};
